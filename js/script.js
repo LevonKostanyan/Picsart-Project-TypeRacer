929a81841f34e08d9d1ff215a4a4c1a262b7803c
yourusername <your@email.com>
2022-02-20 00:48:31 +0400

    add WPS and WPM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const inp = document.querySelector("input");
+const wpm = document.querySelector(".wpm");
+const wps = document.querySelector(".wps");
+const changeColor = document.querySelector('.btn-left');
+const tryAgainBtn = document.querySelector(".try-again")
+const timerEl = document.getElementById("remaining-time");
 const textForTyping = document.querySelector(".text-for-typing")
+
 const pharagraphs = [`The oldest classical Greek and Latin writing had little or no space between words and could be written in boustrophedon (alternating directions).`,
     `I had a painting teacher once tell me that, the difference between a good painting and a great painting, is typically five strokes. And they're usually the five boldest strokes in the painting. The question, of course, is which five strokes?`,
     `Ever since I was a child, folks have thought they had me pegged, because of the way I am, the way I talk. And they're always wrong.`
@@ -7,13 +13,11 @@ const pharagraphs = [`The oldest classical Greek and Latin writing had little or
 //Generate random text from pharagraphs[]
 const text = pharagraphs[parseInt(Math.random() * pharagraphs.length)];
 
+
 //timer
-const timerEl = document.getElementById("remaining-time");
-let wpm = document.querySelector(".wpm");
-let wps = document.querySelector(".wps");
 let timer,
     minutes = 0;
-seconds = 0;
+    seconds = 0;
 
 const taskTimer = () => {
     const now = new Date();
@@ -49,13 +53,12 @@ function onInput() {
         taskTimer();
     }
     for (let i in inpArray) {
-        if (inpArray[i] == cursorChar.innerText) {
+        if (inpArray[i] === cursorChar.innerText) {
             isCorrect = true;
             cursorChar.classList.remove("cursor")
             cursorChar.classList.add("correct");
             inp.classList.remove("incorrect")
             cursorChar = characters[++charIndex]
-
         }
         else {
             isCorrect = false;
@@ -64,6 +67,11 @@ function onInput() {
         }
         if (charIndex >= characters.length) {
             //End Game
+            const numberOfWords = text.split(" ").length;
+            const wordPerSeconde = numberOfWords / seconds
+            wps.innerText = ` WPS - ${wordPerSeconde.toFixed(1)}`
+            const wordPerMinute = wordPerSeconde * 60
+            wpm.innerText = ` WPM - ${wordPerMinute.toFixed(1)}`
             inp.value = null;
             inp.removeEventListener('input', onInput);
             clearInterval(timer);
@@ -76,4 +84,24 @@ function onInput() {
     }
     cursorChar.classList.add("cursor")
 }
+
 inp.addEventListener('input', onInput);
+
+//Try again
+function resetGame() {
+    window.location.reload();
+}
+
+tryAgainBtn.addEventListener('click', resetGame);
+
+
+//Change Theme and save it in local storage
+
+if (!localStorage.theme) localStorage.theme = "light"
+document.body.className = localStorage.theme
+
+changeColor.addEventListener('click', () => {
+    document.body.classList.toggle("dark-mode");
+    localStorage.theme = document.body.className || "light"
+})
+
